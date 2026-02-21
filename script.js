d5b66cd0266e1038d84c95fcaa42463bf16c2abb
shafikulnayem10 <inayem98@gmail.com>
2026-02-21 22:26:04 +0600

    implement btns events

--- script.js
+++ script.js
@@ -41,6 +41,108 @@ function toggleStyle(id) {
     const selected = document.getElementById(id);
     selected.classList.remove('bg-gray-300');
     selected.classList.add('bg-blue-600','text-white');
+     currentStatus = id;
+
+    if (id === 'all-filter-btn') {
+        allCardSection.classList.remove('hidden');
+        filterSection.classList.add('hidden');
+        calculateCount();
+    }
+    else if (id === 'interview-filter-btn') {
+        allCardSection.classList.add('hidden');
+        renderList(interviewList);
+    }
+    else if (id === 'rejected-filter-btn') {
+        allCardSection.classList.add('hidden');
+        renderList(rejectedList);
+    }
 
 
+}
+
+
+
+mainContainer.addEventListener('click', function (event) {
+
+    
+    const card = event.target.closest('.card');
+    if (!card){
+        return;
+    }
+    
+    const company = card.querySelector('.company').innerText;
+    const position = card.querySelector('.position').innerText;
+    const salary = card.querySelector('.salary').innerText;
+
+    if (event.target.classList.contains('interview-btn')) {
+
+        
+        card.querySelector('.status').innerText = "Interview";
+
+       
+        const alreadyExist = interviewList.find(job => job.company === company);
+
+        if (!alreadyExist) {
+            interviewList.push({
+                company: company,
+                position: position,
+                salary: salary,
+                status: "Interview"
+            });
+        }
+
+        rejectedList = rejectedList.filter(job => job.company !== company);
+
+        calculateCount();
+    }
+
+
+   
+    else if (event.target.classList.contains('rejected-btn')) {
+
+        card.querySelector('.status').innerText = "Rejected";
+
+        const alreadyExist = rejectedList.find(job => job.company === company);
+
+        if (!alreadyExist) {
+            rejectedList.push({
+                company: company,
+                position: position,
+                salary: salary,
+                status: "Rejected"
+            });
+        }
+
+        interviewList = interviewList.filter(job => job.company !== company);
+
+        calculateCount();
+    }
+
+
+
+    else if (event.target.classList.contains('btn-delete')) {
+
+        
+        interviewList = interviewList.filter(job => job.company !== company);
+        rejectedList = rejectedList.filter(job => job.company !== company);
+
+        card.remove();
+
+        calculateCount();
+    }
+
+
+  
+    if (currentStatus === 'interview-filter-btn') {
+        renderList(interviewList);
+    }
+
+    if (currentStatus === 'rejected-filter-btn') {
+        renderList(rejectedList);
+    }
+
+});
+function renderList(list) {
+    filterSection.classList.remove('hidden');
+    filterSection.innerHTML = '<h1 >hello</h1>';
 }
